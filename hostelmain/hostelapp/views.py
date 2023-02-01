5e03fef9c4b818ccedf409ce1e0538500d142e16
shakil-py <shakilhaquepy@gmail.com>
2023-02-01 14:46:37 +0600

    add

--- hostelmain/hostelapp/views.py
+++ hostelmain/hostelapp/views.py
@@ -27,6 +27,7 @@ def aftersubmit(request):
         return millrate
 
     millrate = cal()
+    def totalmealreat():
 
     def masage():
         if millrate > 14:
@@ -40,26 +41,6 @@ def aftersubmit(request):
     return render(request, "resultindex.html", millrate)
 
 
-# @csrf_exempt
-# def mealview(request):
-#     shakil = request.GET.get("shakil", False)
-#     sadik = request.GET.get("sadik", False)
-#     babu = request.GET.get("babu", False)
-
-#     if request.method == 'POST':
-#         form = RadioButtonForm(request.POST)
-#         if form.is_valid():
-#             shakil = form.cleaned_data["shakil"]
-#             sadik = form.cleaned_data["sadik"]
-#             babu = form.cleaned_data["babu"]
-#             meal = Meal(person_1=shakil, person_2=sadik, person_3=babu)
-#             meal.save()
-#             return render(request, 'form.html', {'form': form, 'shakil': shakil, 'sadik': sadik, 'babu': babu})
-#     else:
-#         form = RadioButtonForm()
-#         meal = meal(person_1=shakil, person_2=sadik, person_3=babu)
-#         meal.save()
-#     return render(request, 'form.html', {'form': form})
 @csrf_exempt
 def mealview(request):
     if request.method == 'POST':
